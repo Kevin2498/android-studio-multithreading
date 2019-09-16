342c0704ec5225b1bbb009e654af142bb17dfc08
Kevin Mathew <35139938+Kevin2498@users.noreply.github.com>
2019-09-16 19:37:33 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.a8exp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity
+{
+    ImageView img;
+    Button b1,b2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = findViewById(R.id.button1);
+        b2= findViewById(R.id.button2);
+        img = findViewById(R.id.imageView);
+
+        b1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                new Thread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        img.post(new Runnable()
+                        {
+                            @Override
+                            public void run()
+                            {
+                                img.setImageResource(R.drawable.image1);
+                            }
+                        });
+                    }
+                }).start();
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                new Thread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        img.post(new Runnable()
+                        {
+                            @Override
+                            public void run()
+                            {
+                                img.setImageResource(R.drawable.image2);
+                            }
+                        });
+                    }
+                }).start();
+            }
+        });
+    }
+}
